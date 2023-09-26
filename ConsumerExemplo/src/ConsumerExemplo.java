312aecfa72d681a6ed8d4663bfb9d42fc4ef0b73
nias <netimcunha@gmail.com>
2023-09-26 16:01:22 -0300

    exemplo de Consumer

--- ConsumerExemplo/src/ConsumerExemplo.java
+++ ConsumerExemplo/src/ConsumerExemplo.java
@@ -1,5 +1,16 @@
+import java.util.List;
+import java.util.Array;
+import java.util.function.Consumer;
+
+
 public class ConsumerExemplo {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        List<Integer> numeros =Array.aslist(1,2,3,4,5);
+        Consumer<Integer> imprimirNumeroPar=numero ->{
+            if (numero%2==0){
+                system.out.println(numero);
+            }
+        }
+        numeros.stream().forEach(imprimirNumeroPar);
     }
 }
